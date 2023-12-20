682f6d77adc3b4aca1e712a4b9bcf6abdc684efc
shingo2000 <>
2023-12-20 16:55:35 +0900

    カラー定義置換の判定を変更

--- code.ts
+++ code.ts
@@ -4,24 +4,19 @@ figma.codegen.on("generate", async ({ node }) => {
     const cssProps = await node.getCSSAsync();
     let code = ""
     for (const [key, value] of Object.entries(cssProps)) {
+      console.log([key,value])
 
-      if(key == 'color' || key == 'background'){
-
-        // Color変数を抜き出し
-        let regex = /var\((--\w+(-\w+)?)/;
-        let match = value.match(regex);
-        let _value;
-        if(match){
-          // Color定義のフォーマットを書き換え
-          _value = match[1].replace(/--(\w+)/, "$$$1");
-        }else{
-          _value = value;
-        }
-        code += `${key}: ${_value}\n`
-
+      // Color変数を抜き出し
+      let regex = /var\((--\w+(-\w+)?)/;
+      let match = value.match(regex);
+      let _value;
+      if(match){
+        // Color定義のフォーマットを書き換え
+        _value = match[1].replace(/--(\w+)/, "$$$1");
       }else{
-        code += `${key}: ${value}\n`
+        _value = value;
       }
+      code += `${key}: ${_value}\n`
     }
     return [
       {
